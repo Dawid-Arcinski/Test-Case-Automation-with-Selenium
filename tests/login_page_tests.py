9b7b1dad6a00e02c3d14ca6d7e906cc09bbdaa52
Dawid-Arcinski <dawidarcinski@protonmail.com>
2022-05-22 12:05:57 +0200

    moved login tests case documentation back in test case file

--- tests/login_page_tests.py
+++ tests/login_page_tests.py
@@ -7,6 +7,20 @@ from pages.locators import PrimaryHeaderLocators
 class LoginPageTest(BaseTest):
 
     def test_tc001_loging_in_using_correct_credentials(self):
+        """
+        TC001: loging in using correct credentials
+
+        PRECONDITIONS:
+        1. browser opened on website https://www.saucedemo.com/
+
+        STEPS:
+        1. enter correct username
+        2. enter correct password
+        3. click login button
+
+        EXPECTED RESULTS:
+        1. user is logged in
+        """
         driver = self.driver
         login_page = LoginPage(driver)
         login_page.log_user_in(self.test_data["username"], self.test_data["password"])
@@ -15,6 +29,21 @@ class LoginPageTest(BaseTest):
         assert logout_link.accessible_name == self.test_data["logout_label"]
 
     def test_tc002_loging_in_using_wrong_username(self):
+        """
+        TC002: loging in using wrong username
+
+        PRECONDITIONS:
+        1. browser opened on website https://www.saucedemo.com/
+
+        STEPS:
+        1. enter wrong username
+        2. enter correct password
+        3. click login button
+
+        EXPECTED RESULTS:
+        1. user is not logged in
+        2. webpage displays message 'Epic sadface: Username and password do not match any user in this service'
+        """
         driver = self.driver
         login_page = LoginPage(driver)
         login_page.log_user_in(self.test_data["wrong_username"], self.test_data["password"])
@@ -22,6 +51,21 @@ class LoginPageTest(BaseTest):
         assert error_message == self.test_data["login_error_message"]
 
     def test_tc003_loging_in_using_wrong_password(self):
+        """
+        TC003: loging in using wrong password
+
+        PRECONDITIONS:
+        1. browser opened on website https://www.saucedemo.com/
+
+        STEPS:
+        1. enter correct username
+        2. enter wrong password
+        3. click login button
+
+        EXPECTED RESULTS:
+        1. user is not logged in
+        2. webpage displays message 'Epic sadface: Username and password do not match any user in this service'
+        """
         driver = self.driver
         login_page = LoginPage(driver)
         login_page.log_user_in(self.test_data["username"], self.test_data["wrong_password"])
